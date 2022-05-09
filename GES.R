79dea1531103e66a5fae6716c07b7956c850a978
Alejandro Villegas <ale.villegas@uc.cl>
2022-05-09 08:48:22 -0400

    Lee archivo original de sigges

--- GES.R
+++ GES.R
@@ -8,8 +8,22 @@ Fecha <- Sys.Date()
 
 # Fecha <- "2021-01-31" #por si tengo que usar una fecha manual
 
-vigentes_semana <- read_excel("C:/Users/control.gestion3/OneDrive/BBDD Produccion/GES/CSV/Datos semanales/GES Vigentes.xlsx")
-vencidas_semana <- read_excel("C:/Users/control.gestion3/OneDrive/BBDD Produccion/GES/CSV/Datos semanales/GES Vencidas.xlsx")
+# vigentes_semana <- read_excel("C:/Users/control.gestion3/OneDrive/BBDD Produccion/GES/CSV/Datos semanales/GES Vigentes.xlsx")
+# vencidas_semana <- read_excel("C:/Users/control.gestion3/OneDrive/BBDD Produccion/GES/CSV/Datos semanales/GES Vencidas.xlsx")
+
+vigentes_semana <- read_excel("C:/Users/control.gestion3/OneDrive/BBDD Produccion/GES/CSV/Datos semanales/vigentes_sigges.xlsx", 
+                              col_types = c("text", "skip", "skip", 
+                                            "skip", "skip", "skip", "numeric", 
+                                            "numeric", "numeric", "text", "skip"),
+                              skip = 7)
+vigentes_semana <- na.omit(vigentes_semana)
+
+vencidas_semana <- read_excel("C:/Users/control.gestion3/OneDrive/BBDD Produccion/GES/CSV/Datos semanales/vencidas_sigges.xlsx", 
+              col_types = c("skip", "skip", "skip", 
+                            "skip", "skip", "text", "text", "numeric", 
+                            "numeric", "numeric", "text", "skip"), 
+              skip = 7)
+vencidas_semana <-na.omit(vencidas_semana)
 
 
 vigentes_semana <- janitor::clean_names(vigentes_semana)
